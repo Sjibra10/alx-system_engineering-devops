64627f31d9d957523da52627e0bec969af4625bb
‘Sjibra10’ <buhleobasibuka@gmail.com>
2024-06-04 12:32:21 -0700

    fixing errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,23 @@
 """main file for subs"""
 
 import requests
-headers = {"User-Agent": "MyRedditBot/1.0"}
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, the function should return 0.
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If an invalid subreddit is given,
+    the function should return 0.
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'python:subscribers:v1.0 (by /u/yourusername)'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get('data', {})
+            return data.get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
