828e48d41f550a0d94800752e35ec68da6b5c95e
Buhle Buka <142313755+Sjibra10@users.noreply.github.com>
2024-06-29 14:36:18 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-hange ULIMIT value from 15 to 2000
+#Change/making the value of ulimit from 15 to 2000
+
 exec {'/etc/default/nginx':
 command  => 'sed -i "s/15/500/g" /etc/default/nginx; sudo service nginx restart',
 provider => shell,
-}
